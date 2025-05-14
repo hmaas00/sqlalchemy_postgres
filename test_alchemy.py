d22fd367ae7086c3462d9cf11e8cf3e78bb301ed
hmaas00 <hmaas00@gmail.com>
2025-05-14 20:23:42 -0300

    funcionando insert many to many

--- test_alchemy.py
+++ test_alchemy.py
@@ -4,10 +4,11 @@ from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy.orm import Session
 
 from dotenv import dotenv_values
 
-from typing import Optional
+from typing import Optional, List
 
 
 if __name__ == '__main__':
@@ -32,7 +33,8 @@ if __name__ == '__main__':
         empregado_id: Mapped[int] = mapped_column(ForeignKey("mytest.Empregado.id"), primary_key=True) # mytest.Empregado.id (schema.table.column)
         projeto_id: Mapped[int] = mapped_column(ForeignKey("mytest.Projeto.id"), primary_key=True) # mytest.Projeto.id (schema.table.column)
         obsevacao: Mapped[Optional[str]]
-        projetos: Mapped["Projeto"] = relationship()
+        projeto: Mapped["Projeto"] = relationship(back_populates="empregados")
+        empregado: Mapped["Empregado"] = relationship(back_populates="projetos")
     
     # an example mapping using the base
     class Empregado(Base):
@@ -40,14 +42,27 @@ if __name__ == '__main__':
         __table_args__ = {"schema": 'mytest'}
         id: Mapped[int] = mapped_column(primary_key=True)
         name: Mapped[str] = mapped_column(String(30))
+        projetos: Mapped[List["Empregado_Projeto"]] = relationship(back_populates="empregado")
 
     class Projeto(Base):
         __tablename__ = "Projeto"
         __table_args__ = {"schema": 'mytest'}
         id: Mapped[int] = mapped_column(primary_key=True)
         name: Mapped[str] = mapped_column(String(30))
+        empregados: Mapped[List["Empregado_Projeto"]] = relationship(back_populates="projeto")
 
     # criacao da base
     engine = create_engine(url)
     Base.metadata.create_all(engine)
+
+    # criar um objeto na base
+    with Session(engine) as session:
+        empregado_projeto = Empregado_Projeto()
+        john = Empregado(name="Smith")
+        projeto_a = Projeto(name='projeto abc')
+        empregado_projeto.empregado = john
+        empregado_projeto.projeto = projeto_a
+        empregado_projeto.obsevacao = "projeto bom"
+        session.add(john)
+        session.commit()
     pass
