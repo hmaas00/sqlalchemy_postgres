ec19e8be84649696ea1d1ba27d45705f6e5a3f46
hmaas00 <hmaas00@gmail.com>
2025-05-24 10:05:44 -0300

    separacao da criacao do schema e select orm

--- test_alchemy.py
+++ test_alchemy.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, text, select
+from sqlalchemy import create_engine, text, select, BigInteger
 from sqlalchemy.engine import URL
 from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase
@@ -10,55 +10,60 @@ from dotenv import dotenv_values
 
 from typing import Optional, List
 
+CONFIG = dotenv_values(".env")  # CONFIG = {"USER": "foo", "EMAIL": "foo@example.org"}
 
 
+    # declarative base class
+class Base(DeclarativeBase):
+    pass
 
-if __name__ == '__main__':
-    config = dotenv_values(".env")  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
+class Empregado_Projeto(Base):
+    __tablename__ = "Empregado_Projeto"
+    __table_args__ = {"schema": CONFIG['POSTGRES_SCHEMA']}
+    empregado_id: Mapped[int] = mapped_column(ForeignKey(f"{CONFIG['POSTGRES_SCHEMA']}.Empregado.id"), primary_key=True) # {CONFIG['POSTGRES_SCHEMA']}.Empregado.id (schema.table.column)
+    projeto_id: Mapped[int] = mapped_column(BigInteger, ForeignKey(f"{CONFIG['POSTGRES_SCHEMA']}.Projeto.id"), primary_key=True) # {CONFIG['POSTGRES_SCHEMA']}.Projeto.id (schema.table.column)
+    obsevacao: Mapped[Optional[str]]
+    projeto: Mapped["Projeto"] = relationship(back_populates="empregados")
+    empregado: Mapped["Empregado"] = relationship(back_populates="projetos")
 
+# an example mapping using the base
+class Empregado(Base):
+    __tablename__ = "Empregado"
+    __table_args__ = {"schema": CONFIG['POSTGRES_SCHEMA']}
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    projetos: Mapped[List["Empregado_Projeto"]] = relationship(back_populates="empregado")
+
+class Projeto(Base):
+    __tablename__ = "Projeto"
+    __table_args__ = {"schema": CONFIG['POSTGRES_SCHEMA']}
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    empregados: Mapped[List["Empregado_Projeto"]] = relationship(back_populates="projeto")
+
+def init_engine():
     url = URL.create(
         drivername="postgresql",
-        username= config['POSTGRES_USER'],
-        password= config['POSTGRES_PW'],
+        username= CONFIG['POSTGRES_USER'],
+        password= CONFIG['POSTGRES_PW'],
         host="localhost",
         port=5432,
         database="postgres"
     )
-
-    # declarative base class
-    class Base(DeclarativeBase):
-        pass
-
-    class Empregado_Projeto(Base):
-        __tablename__ = "Empregado_Projeto"
-        __table_args__ = {"schema": 'mytest'}
-        empregado_id: Mapped[int] = mapped_column(ForeignKey("mytest.Empregado.id"), primary_key=True) # mytest.Empregado.id (schema.table.column)
-        projeto_id: Mapped[int] = mapped_column(ForeignKey("mytest.Projeto.id"), primary_key=True) # mytest.Projeto.id (schema.table.column)
-        obsevacao: Mapped[Optional[str]]
-        projeto: Mapped["Projeto"] = relationship(back_populates="empregados")
-        empregado: Mapped["Empregado"] = relationship(back_populates="projetos")
     
-    # an example mapping using the base
-    class Empregado(Base):
-        __tablename__ = "Empregado"
-        __table_args__ = {"schema": 'mytest'}
-        id: Mapped[int] = mapped_column(primary_key=True)
-        name: Mapped[str] = mapped_column(String(30))
-        projetos: Mapped[List["Empregado_Projeto"]] = relationship(back_populates="empregado")
+    engine = create_engine(url)
 
-    class Projeto(Base):
-        __tablename__ = "Projeto"
-        __table_args__ = {"schema": 'mytest'}
-        id: Mapped[int] = mapped_column(primary_key=True)
-        name: Mapped[str] = mapped_column(String(30))
-        empregados: Mapped[List["Empregado_Projeto"]] = relationship(back_populates="projeto")
+    return engine
+
+def gen_schema():
+
+    engine = init_engine(CONFIG)
 
     # criacao da base
-    engine = create_engine(url)
+    
     Base.metadata.create_all(engine)
 
-
-    def gen_empregado(emp_nome, proj_nome, proj_obs):
+def gen_empregado(emp_nome, proj_nome, proj_obs):
         empregado_projeto = Empregado_Projeto()
         emp = Empregado(name=emp_nome)
         projeto_a = Projeto(name=proj_nome)
@@ -67,14 +72,18 @@ if __name__ == '__main__':
         empregado_projeto.obsevacao = proj_obs
         return emp
 
+if __name__ == '__main__':
+
+    engine = init_engine()
 
     # criar um objeto na base
     with Session(engine) as session:
         # emp = gen_empregado('jose', 'alpha', 'secreto')
         # session.add(emp)
-        res = session.execute(text('SELECT id, nome FROM mytest."OutAlchemy";'))
+        # res = session.query(Projeto).filter(Projeto.name=='projeto A')
+        res = session.query(Projeto)
         for r in res:
-            print(f'\n{r}')
+            print(f'\n{r.id} - {r.name}')
 
         #stmt = select(User).where(User.name.in_(["spongebob", "sandy"]))
 
